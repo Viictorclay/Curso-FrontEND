276e83d96ba3c51b4c069db57162319794bd6c9b
Viictorclay <197065619+Viictorclay@users.noreply.github.com>
2025-09-08 21:41:49 -0300

    Desafio Boas-vidas
    Desafio feito — 08/09/2025
    Proposto pela Prof. Josiane Lopes
    Curso: Front-END com React - Capacita IFS
    Mês: 2/ 6

---
+++ Desafios/Desafio das Mensagens/script.js
@@ -0,0 +1,14 @@
+const botao01 = document.getElementById('btm01')
+botao01.addEventListener('click', function() {
+    alert('Você clicou no 1° botão!');
+});
+
+const botao02 = document.getElementById('btm02')
+botao02.addEventListener('click', function() {
+    alert('Você aclicou no 2° botão!')
+});
+
+const botao03 = document.getElementById('btm03')
+botao03.addEventListener('click', function() {
+    alert('Você clicou no 3° botão!')
+})
