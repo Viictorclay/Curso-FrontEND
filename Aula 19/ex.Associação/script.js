061ebed8363bcb094b31a7db39452ad2f3ae8f0c
Viictorclay <197065619+Viictorclay@users.noreply.github.com>
2025-10-18 16:33:36 -0300

    EXERCÍCIOS DE ASSOCIAÇÃO E COMPOSIÇÃO
    Desafio feito — 18/10/2025
    Proposto pela Prof. Josiane Lopes
    Material da Slide — PDF
    Curso: Front-END com React - Capacita IFS
    Mês: 3/ 6

---
+++ Aula 19/ex.Associação/script.js
@@ -0,0 +1,58 @@
+// Primeiro declaro o que será associado
+class Disciplina {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+
+// Segundo declaro a 'Classe que contém a associação'
+class Professor {
+    constructor(nome) {
+        this.nome = nome;
+        this.disciplina = null; // associação
+    }
+
+    // Método para associar as duas classes
+    associar(disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    // Método para exibir as Informações com efeito de processamento
+    exibirInfo() {
+            if (this.disciplina) { // o 'this.nome' sempre será o da classe
+                process.stdout.write('vinculando') // escreve sem pular linha
+
+                let pontos = 0;
+                const animacao = setInterval(() => {
+                    process.stdout.write('.');
+                    pontos++;
+                    if ( pontos === 5) {
+                        clearInterval(animacao);
+                        console.log('\n');
+                        setTimeout(() => {
+                            console.log('================================================================')
+                            console.log(`👩‍🏫 Professora: ${this.nome} \n📘 Disciplina: ${this.disciplina.nome} \n🟢 Status: Associado com sucesso!` ); // para colocar nome de fora da classe 'this.*nomeDoAssociado*.nome
+                            console.log('================================================================\n')
+                        },500);
+                    }
+                }, 500); // meio segundo entre cada ponto
+                
+            } else {
+                console.log('================================================================')
+                console.log(`👩‍🏫 Professora: ${this.nome} \n⚠️ Status: Não está vinculado(a) a nenhuma disciplina no momento.`)
+                console.log('================================================================\n')
+        }
+       
+    }
+}
+
+// Exemplo de uso
+
+const prof1 = new Professor('Beatriz Marinheiro')
+const disc1 = new Disciplina('Redes de Computadores')
+
+prof1.exibirInfo(); // Antes de associar
+setTimeout(() => {
+    prof1.associar(disc1);
+    prof1.exibirInfo(); // Depois de associar
+}, 2000)
