32303e446b30f7fddd2d6c7708a6e140058cfa07
Viictorclay <197065619+Viictorclay@users.noreply.github.com>
2026-02-09 22:00:53 -0300

    Atualizando o Server

--- Aula04-WebServices/chat-backend/server.js
+++ Aula04-WebServices/chat-backend/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://localhost:3000"],
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
@@ -43,6 +43,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3001, () => {
-    console.log("🚀 Servidor rodando em http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
